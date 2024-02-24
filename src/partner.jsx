9e6a09e871f86a9f18ddf805f60b034fa2c11ce2
parker jones <parkerjones1036@gmail.com>
2024-02-23 21:37:39 -0500

    update6

--- src/partner.jsx
+++ src/partner.jsx
@@ -14,44 +14,27 @@ const Partner = () => {
   );
 };
 const Grid = () => {
+  const GridText = [
+    "Doosfot",
+    "PizzaGame",
+    "Playfair",
+    "ABND",
+    "Microby",
+    "IOU",
+    "Orange",
+    "Mitsy",
+    "Exprezz",
+    "Beru Eats",
+    "Quantik",
+    "Riffmax",
+  ];
   return (
     <div className="lg:grid lg:w-full w-[90%] lg:grid-cols-4 grid grid-cols-2 gap-4">
-      <div className="w-full h-[129px] bg-[#1a2737]  text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Doosfot</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>PizzaGame</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Playfair</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>ABND</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Microby</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>IOU</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Orange</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Mitsy</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Exprezz</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Beru Eats</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Quantik</p>
-      </div>
-      <div className="w-full h-[129px] bg-[#1a2737] text-[18px] lg:text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
-        <p>Riffmax</p>
-      </div>
+      {GridText.map((item) => (
+        <div className="w-full h-[129px] bg-[#1a2737]  text-2xl text-white font-bold flex items-center justify-center rounded-xl cursor-pointer">
+          <p>{item}</p>
+        </div>
+      ))}
     </div>
   );
 };
